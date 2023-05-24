d39670cb9f885214d5a5b55c38e59333b321200a
Naomi Shigemori <mnaomix@gmail.com>
2023-05-24 11:36:11 +0900

    Change value of devtool property

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,7 +3,8 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map',
+  // source-map, nosources-source-map, hidden-nosources-source-map, hidden-source-map
+  devtool: 'source-map',
   devServer: {
     static: './docs',
   },
